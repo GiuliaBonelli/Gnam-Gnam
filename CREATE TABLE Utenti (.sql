ce6761d22adb20d030ba2cabc84679c5b4120341
Eryper <egovoni44@gmail.com>
2024-04-04 17:28:48 +0200

    GNAM GNAM 3

---
+++ CREATE TABLE Utenti (.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Utenti (
+    ID_UTENTE      NOT NULL
+                   PRIMARY KEY,
+    NOME           NOT NULL,
+    COGNOME        NOT NULL,
+    BIRTH     DATE NOT NULL,
+    INDIRIZZO      NOT NULL,
+    CARTA          NOT NULL
+);
